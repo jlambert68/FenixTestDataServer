80ce44bdd6d04db5ecfb3c01cfbce63086b6e670
jlamber <jonas.lambert@drakon.se>
2022-02-03 16:54:49 +0100

    Fixed so it can connect to CloudDB from Cloud Run

--- dbSetup.go
+++ dbSetup.go
@@ -22,20 +22,37 @@ func mustGetenv(k string) string {
 
 func connectToDB() {
 
+	var dbURI string
+	var err error
+
 	var (
 		dbUser    = mustGetenv("DB_USER") // e.g. 'my-db-user'
 		dbPwd     = mustGetenv("DB_PASS") // e.g. 'my-db-password'
 		dbTCPHost = mustGetenv("DB_HOST") // e.g. '127.0.0.1' ('172.17.0.1' if deployed to GAE Flex)
 		dbPort    = mustGetenv("DB_PORT") // e.g. '5432'
 		dbName    = mustGetenv("DB_NAME") // e.g. 'my-database'
+
 	)
 
-	dbURI := fmt.Sprintf("host=%s user=%s password=%s port=%s database=%s", dbTCPHost, dbUser, dbPwd, dbPort, dbName)
+	// If the optional DB_HOST environment variable is set, it contains
+	// the IP address and port number of a TCP connection pool to be created,
+	// such as "127.0.0.1:5432". If DB_HOST is not set, a Unix socket
+	// connection pool will be created instead.
+	if dbTCPHost != "GCP" {
+		dbURI = fmt.Sprintf("host=%s user=%s password=%s port=%s database=%s", dbTCPHost, dbUser, dbPwd, dbPort, dbName)
 
-	// Postgres version 14
-	//dbConnectionURL := "postgres://caxdbuser:password@127.0.0.1:5432/caxdb"
+	} else {
 
-	var err error
+		var dbInstanceConnectionName = mustGetenv("DB_INSTANCE_CONNECTION_NAME")
+
+		socketDir, isSet := os.LookupEnv("DB_SOCKET_DIR")
+		if !isSet {
+			socketDir = "/cloudsql"
+		}
+
+		dbURI = fmt.Sprintf("user=%s password=%s database=%s host=%s/%s", dbUser, dbPwd, dbName, socketDir, dbInstanceConnectionName)
+
+	}
 
 	DbPool, err = pgxpool.Connect(context.Background(), dbURI)
 	if err != nil {
